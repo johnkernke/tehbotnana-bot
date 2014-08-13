c8dcc3f82de620f3520ee3806c5db8d3bdbf926a
John Kernke <john.kernke@gmail.com>
2014-08-13 18:54:53 +1000

    Updated twitter to support multiple channels and accounts per channel

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 app = {};
 app.config = require('./config.js');
+
+app.config.irc.autoJoinChannels = [];
+for (var channel in app.config.channels) {
+    app.config.irc.autoJoinChannels.push(channel);
+}
+
 app.irc = new (require('./lib/irc'))(app.config.irc);
 
 new (require('./lib/irc_twitter'))();
 
-// need to really work out handling this better
+// need to really work out handling this better, also need to make it detect disconnects better
 app.irc.on('disconnect', function () {
   setTimeout(app.irc.connect, 60000);
 });
--- config.sample.js
+++ config.sample.js
@@ -3,25 +3,36 @@ module.exports = {
         host: 'irc.server.com',
         port: 6667,
         nick: 'TehBotnana',
-        autoJoinChannels: ['#TehBotnana'],
         log: 'notice'
     },
-    twitter: {
-        auth: {
-            consumer_key: '',
-            consumer_secret: '',
-            access_token_key: '',
-            access_token_secret: ''
-        },
-        channels: {
-            "#TehBotnana": {
-                twitter: "tehbanana_",
+    channels: {
+        "#tehbotnana": {
+            twitter: {
+                accounts: ["tehbanana_"],
                 blocked: {
                     users: ["blockeduser"],
                     content: ["blockedcontent"]
                 }
             }
         },
-        verbose: true
-    }
+        "#tehbanana": {
+            twitter: {
+                accounts: ["tehbanana_"],
+                blocked: {
+                    users: ["blockeduser"],
+                    content: ["blockedcontent"]
+                }
+            }
+        },
+        "#random": {
+
+        }
+    },
+    twitter: {
+        consumer_key: '',
+        consumer_secret: '',
+        access_token_key: '',
+        access_token_secret: ''
+    },
+    twitter_verbose: true // need to make this nicer, without passing through to ntwitter.. use similar method to IRC, maybe join the config setting?
 }
